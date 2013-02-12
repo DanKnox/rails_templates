46677d7691b693d253203975c607a419b7e124ff
Dan Knox <dknox@visiontree.com>
2013-02-11 16:37:57 -0800

    Add base template.

---
+++ base.rb
@@ -0,0 +1,48 @@
+# Add my most frequently used Gems
+gem "thin"
+
+gem_group :development, :test do
+  gem "rspec-rails"
+  gem "factory_girl_rails"
+  gem "shoulda-matchers"
+  gem "guard"
+  gem "guard-rspec"
+  gem "guard-bundler"
+  gem "rb-inotify"
+end
+
+run "bundle install"
+run "bundle exec rails g rspec:install"
+
+# Git rid of the annoying default files
+run "rm README.rdoc"
+run "rm public/index.html"
+
+# Add some sensible default files
+file "app/controllers/public_controller.rb", <<-CODE
+class PublicController < ApplicationController
+
+  def index
+  end
+
+end
+CODE
+
+file "app/views/public/index.html.erb", <<-CODE
+<h1>Home</h1>
+CODE
+
+app_name = ask("New project name:")
+file "README.md", <<-MARKDOWN
+# #{app_name}
+
+Application description will go here.
+MARKDOWN
+
+# Set up the default route
+route "root to: 'person#index'"
+
+# Create the Git repository
+git :init
+git add: "."
+git commit: "-a -m 'We have lift off.'"
